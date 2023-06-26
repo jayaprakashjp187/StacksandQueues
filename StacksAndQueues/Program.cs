37d13230c9815a97bd9076bdae1624f4425f6c40
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-26 21:47:53 +0530

    [jayaprakash by using pop and peek method until the values are empty]

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace StacksAndQueues
 {
@@ -27,6 +28,28 @@ namespace StacksAndQueues
 
                 
                 break;
+                    case 2:
+                    Console.WriteLine();
+                    Stack<int> s = new Stack<int>();
+
+                    // Pushing elements to the stack
+                    s.Push(70);
+                    s.Push(30);
+                    s.Push(56);
+
+                    Console.WriteLine("Stack elements before popping:");
+
+                    // Peek and pop until the stack is empty
+                    while (s.Count > 0)
+                    {
+                        int topElement = s.Peek();
+                        Console.WriteLine("Peeked element: " + topElement);
+
+                        int poppedElement = s.Pop();
+                        Console.WriteLine("Popped element: " + poppedElement);
+                    }
+            
+                     break;
                  
            
 
