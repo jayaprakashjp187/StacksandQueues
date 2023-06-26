fcab894bc78da5f2a5a0cea14a25df166c14cb1d
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-26 21:32:13 +0530

    [jayaprakash  creating a stack]

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the stacks and queues program");
+            Console.WriteLine("1.Creating a stack");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                Stack<int> stack = new Stack<int>();
+
+                // Pushing elements to the stack
+                stack.Push(70);
+                stack.Push(30);
+                stack.Push(56);
+
+                Console.WriteLine("Elements in the stack:");
+                foreach (int item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+
+                
+                break;
+                 
+           
+
+             }
+        }
+    }
+}
